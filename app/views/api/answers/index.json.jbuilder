dc290016464799961ff909af89ded39d11d5e931
Sean OReilly <seanvoreilly2@gmail.com>
2017-07-20 14:52:24 -0700

    prep heroku push

--- app/views/api/answers/index.json.jbuilder
+++ app/views/api/answers/index.json.jbuilder
@@ -1,3 +1,5 @@
 @answers.each do |answer|
-  json.partial! 'api/answers/answer', answer: answer
+  json.set! answer.id do
+    json.partial! 'api/answers/answer', answer: answer
+  end
 end
--- frontend/components/questions/questions_index.jsx
+++ frontend/components/questions/questions_index.jsx
@@ -4,14 +4,13 @@ class QuestionsIndex extends React.Component {
   renderQuestions() {
     return this.props.questions.map((question, idx) => (
       <li>
-        <h1>{ question }</h1>
+        <h1>{ question.title }</h1>
         <p>{}</p>
       </li>
     ));
   }
 
   render() {
-    console.log(this.props.questions);
     return (
       <div>
         <h1>Questions</h1>
