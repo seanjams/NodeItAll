7b7a91f9d7bdde40e4cb9cc884891a464961a235
Sean OReilly <seanvoreilly2@gmail.com>
2017-09-08 13:15:32 -0700

    index items fully clickable

--- frontend/components/questions/question_index_item.jsx
+++ frontend/components/questions/question_index_item.jsx
@@ -6,17 +6,17 @@ class QuestionIndexItem extends React.Component {
   render() {
     const { question } = this.props;
     return (
-      <div className="question-index-item">
-        <ItemCounts question={question} />
-        <div className="question-container">
-          <Link to={`questions/${question.id}`}>
+      <Link to={`questions/${question.id}`}>
+        <div className="question-index-item">
+          <ItemCounts question={question} />
+          <div className="question-container">
             <h2 className="question-title">{question.title}</h2>
-          </Link>
-          <div className="author-info">
-            <p>asked by <span className="username">{question.username}</span> {question.time} ago</p>
+            <div className="author-info">
+              <p>asked by <span className="username">{question.username}</span> {question.time} ago</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
